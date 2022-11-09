c60047bd942436b298792b8aca01159b05d4069a
TheInkedEngineer <firas@hey.com>
2022-11-09 09:57:50 +0100

    Add code

--- DemoApp/App.tsx
+++ DemoApp/App.tsx
@@ -9,72 +9,78 @@
  */
 
  import {
-   Pressable,
-   SafeAreaView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- 
- import {
-   Colors
- } from 'react-native/Libraries/NewAppScreen';
- 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-     flex: 1
-   };
- 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar
-         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-         backgroundColor={backgroundStyle.backgroundColor}
-       />
-       <View
-         style={{
-           flex: 1,
-           justifyContent: 'center'
-         }}>
-         <Pressable style={styles.button}>
-             <Text style={styles.text}> Start SDK </Text>
-         </Pressable>
-         <Pressable style={styles.button}>
-             <Text style={styles.text}> Process Single Payment </Text>
-         </Pressable>
-         <Pressable style={styles.button}>
-             <Text style={styles.text}> Process Mandate </Text>
-         </Pressable>
-       </View>
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
-   button: {
-     alignItems: 'center',
-     paddingVertical: 12,
-     marginHorizontal: 32,
-     marginBottom: 12,
-     borderRadius: 4,
-     backgroundColor: 'black',
-   },
-   text: {
-     fontSize: 16,
-     lineHeight: 21,
-     fontWeight: 'bold',
-     letterSpacing: 0.25,
-     color: 'white',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
- 
- export default App;
- 
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+import RTNTrueLayerPaymentsSDK from 'react-native-true-layer-payments-sdk/js/NativeTrueLayerPaymentsSDK';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center'
+        }}>
+        <Pressable style={styles.button}>
+            <Text style={styles.text}> Start SDK </Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+            <Text style={styles.text}> Process Single Payment </Text>
+        </Pressable>
+        <Pressable style={styles.button} 
+        onPress={()=> { 
+          RTNTrueLayerPaymentsSDK?.configureSDK()
+          console.log(RTNTrueLayerPaymentsSDK?.configureSDK())
+          console.log("HI")
+        }} >
+            <Text style={styles.text}> Process Mandate </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    paddingVertical: 12,
+    marginHorizontal: 32,
+    marginBottom: 12,
+    borderRadius: 4,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
--- RNTrueLayerPaymentsSDK/index.js
+++
@@ -1,6 +0,0 @@
-
-import { NativeModules } from 'react-native';
-
-const { RNTrueLayerPaymentsSdk } = NativeModules;
-
-export default RNTrueLayerPaymentsSdk;
