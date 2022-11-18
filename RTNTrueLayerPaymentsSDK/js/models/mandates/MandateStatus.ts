8a3dddff14a4c054e35c99a693884a8ff071a022
Lukasz Franieczek <lukasz.franieczek@truelayer.com>
2022-11-18 15:19:05 +0000

    Added Mandate and Payment statuses

--- RTNTrueLayerPaymentsSDK/js/models/mandates/MandateStatus.ts
+++ RTNTrueLayerPaymentsSDK/js/models/mandates/MandateStatus.ts
@@ -1,1 +1,9 @@
-export enum MandateStatus {}
+export enum MandateStatus {
+  AuthorizationRequired = "AuthorizationRequired",
+  Authorized = "Authorized",
+  Authorizing = "Authorizing",
+  Executed = "Executed",
+  Failed = "Failed",
+  Settled = "Settled",
+  Revoked = "Revoked",
+}
