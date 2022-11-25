31629003a5868fca303dd924c63001c0601e136e
Daven Gomes <daven.gomes@truelayer.com>
2022-11-25 14:16:10 +0000

    [PUX-2781] Add callbacks - configure callback works

--- RTNTrueLayerPaymentsSDK/ios/RTNTrueLayerPaymentsSDK.mm
+++ RTNTrueLayerPaymentsSDK/ios/RTNTrueLayerPaymentsSDK.mm
@@ -5,8 +5,38 @@
 
 RCT_EXPORT_MODULE()
 
-- (NSString *)configureSDK {
-    return @"LOGGING FROM SDK";
+- (void)_configure:(NSString *)environment
+           resolve:(RCTPromiseResolveBlock)resolve
+            reject:(RCTPromiseRejectBlock)reject
+{
+  resolve(NULL);
+}
+
+- (void)_processPayment:(JS::NativeTrueLayerPaymentsSDK::Spec_processPaymentPaymentContext &)paymentContext
+             prefereces:(JS::NativeTrueLayerPaymentsSDK::Spec_processPaymentPrefereces &)prefereces
+                resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject {
+  resolve(NULL);
+}
+- (void)_processMandate:(JS::NativeTrueLayerPaymentsSDK::Spec_processMandateMandateContext &)mandateContext
+             prefereces:(JS::NativeTrueLayerPaymentsSDK::Spec_processMandatePrefereces &)prefereces
+                resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject {
+  resolve(NULL);
+}
+- (void)_paymentStatus:(NSString *)paymentId
+         resourceToken:(NSString *)resourceToken
+               resolve:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject {
+  resolve(NULL);
+  
+}
+- (void)_mandateStatus:(NSString *)mandateId
+         resourceToken:(NSString *)resourceToken
+               resolve:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject {
+  resolve(NULL);
+  
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
