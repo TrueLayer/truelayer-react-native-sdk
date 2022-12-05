bfcf3799a49d2a34ffe837aeae0c912ec985a245
Daven Gomes <daven.gomes@truelayer.com>
2022-12-05 15:01:29 +0000

    [PUX-2788] Fix compilation

--- RTNTrueLayerPaymentsSDK/ios/RTNTrueLayerSinglePaymentStatus.h
+++ RTNTrueLayerPaymentsSDK/ios/RTNTrueLayerSinglePaymentStatus.h
@@ -1,5 +1,6 @@
 /// The various statuses in which a payment can be.
 /// These reflect the constants in the React Native spec in `types.ts`.
+typedef NSString *RTNTrueLayerSinglePaymentStatus NS_TYPED_ENUM;
 extern RTNTrueLayerSinglePaymentStatus const RTNTrueLayerSinglePaymentStatusAuthorizationRequired;
 extern RTNTrueLayerSinglePaymentStatus const RTNTrueLayerSinglePaymentStatusAuthorized;
 extern RTNTrueLayerSinglePaymentStatus const RTNTrueLayerSinglePaymentStatusAuthorizing;
