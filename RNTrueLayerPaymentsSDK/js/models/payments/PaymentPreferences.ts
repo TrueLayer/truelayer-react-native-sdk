3fbc046dc493794728a519ea94fbfde97feb3895
Keenen Charles <keenen.charles@truelayer.com>
2024-02-06 12:08:51 +0000

    Handle redirects

--- RNTrueLayerPaymentsSDK/js/models/payments/PaymentPreferences.ts
+++ RNTrueLayerPaymentsSDK/js/models/payments/PaymentPreferences.ts
@@ -2,5 +2,8 @@ import { PaymentUseCase } from "./PaymentUseCase";
 
 export type PaymentPreferences = {
   preferredCountryCode?: string;
+  /**
+  * @deprecated The parameter should not be used
+  */
   paymentUseCase: PaymentUseCase;
 };
