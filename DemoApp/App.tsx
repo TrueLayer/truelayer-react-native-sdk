61136487b9bb01fbb478461607257573db8d0f39
Keenen Charles <keenen.charles@truelayer.com>
2024-02-28 11:09:27 +0000

    Fixed mandate launch

--- DemoApp/App.tsx
+++ DemoApp/App.tsx
@@ -216,7 +216,7 @@ function createAndProcessMandate(): void {
       `id: ${processorContext.id}`,
       `resource_token: ${processorContext.resource_token}`,
     )
-    
+    processMandate(processorContext)
   })
 }
 
